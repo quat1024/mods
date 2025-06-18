c0841ad95798eb17aa4abfac49d99e59a3f8f487
quat1024 <quat@highlysuspect.agency>
2025-06-18 00:32:22 -0400

    almost fix it but neo run configs are still fucked up due to their sourceset bullshit

--- neo-1.21.1/src/modTestmodVersion1_21_1LoaderNeoforge/java/agency/highlysuspect/testmod/neo/TestMod1_1_21_1_Neoforge.java
+++ neo-1.21.1/src/modTestmodVersion1_21_1LoaderNeoforge/java/agency/highlysuspect/testmod/neo/TestMod1_1_21_1_Neoforge.java
@@ -2,6 +2,7 @@ package agency.highlysuspect.testmod.neo;
 
 import agency.highlysuspect.Neo121Shared;
 import agency.highlysuspect.SharedAcrossAllVanilla;
+import agency.highlysuspect.TwentyOne;
 import agency.highlysuspect.testmod.TestMod1;
 import agency.highlysuspect.testmod.Testmod1_1_21_1;
 import net.neoforged.fml.common.Mod;
@@ -9,8 +10,9 @@ import net.neoforged.fml.common.Mod;
 @Mod(TestMod1.MODID)
 public class TestMod1_1_21_1_Neoforge extends Testmod1_1_21_1 {
 	static {
-		System.out.println(SharedAcrossAllVanilla.class);
+		System.out.println(TwentyOne.class);
 		System.out.println(Neo121Shared.class);
+		System.out.println(SharedAcrossAllVanilla.class);
 		
 		LOG.info("XXXXXXXX hello from Testmod1_1_21_1_Neoforge");
 	}
